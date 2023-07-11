59159c3d0bf6c36d52b57ea0670a7665b1f4e537
Dayeon Moon <0217dayun@naver.com>
2023-07-11 12:01:42 +0900

    [chore] 프로젝트 초기 세팅, swiftlint 추가

---
+++ DrivePal/DrivePal/DrivePalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrivePalApp.swift
+//  DrivePal
+//
+//  Created by 제나 on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct DrivePalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
