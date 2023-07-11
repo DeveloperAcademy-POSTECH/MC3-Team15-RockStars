13e5db7f043a6c4b0c6ef33119460c03399d6a19
Dayeon Moon <0217dayun@naver.com>
2023-07-11 23:44:10 +0900

    [feat][#6] 배경씬 위에 비행기 이미지 추가
    - DrivingPalView.swift

--- DrivePal/DrivePal/View/DrivingPalView.swift
+++ DrivePal/DrivePal/View/DrivingPalView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import SpriteKit
 
 struct DrivingPalView: View {
+    private let planeImage = "planeWithShadow"
     var scene: SKScene {
         let scene = BackgroundScene()
         scene.scaleMode = .fill
@@ -18,8 +19,17 @@ struct DrivingPalView: View {
     var body: some View {
         ZStack {
             SpriteView(scene: scene)
+            
+            VStack {
+                Spacer()
+                Image(planeImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: UIScreen.main.bounds.width - 100)
+                    .padding(.vertical)
+            }
         }
-            .ignoresSafeArea()
+        .ignoresSafeArea()
     }
 }
 
