d65ffd092f2aa84c9a32de9e60f4c938f6a993a5
Damagucci-Juice <jsr9874@gmail.com>
2023-07-14 21:57:53 +0900

    [refactor] 코드 정리

--- DrivePal/LocationsHandler.swift
+++ DrivePal/LocationsHandler.swift
@@ -9,49 +9,30 @@ import SwiftUI
 import CoreLocation
 import OSLog
 
-class LocationsHandler: NSObject, ObservableObject {
-    private let locationManager: CLLocationManager
-    private let logger = Logger()
-    
+@MainActor final class LocationsHandler: NSObject, ObservableObject {
+    private lazy var locationManager: CLLocationManager? = nil
+    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "",
+                                category: String(describing: LocationsHandler.self))
+
     private var lastLocation: CLLocation?
     @Published var authorizationStatus: CLAuthorizationStatus = .notDetermined
-    @Published var kilometerPerHour = 0.0
+    @Published var kilometerPerHour = 0
     
     override init() {
-        self.locationManager = CLLocationManager()
         super.init()
-        locationManager.delegate =  self
-        locationManager.requestWhenInUseAuthorization()
-        //TODO: - 백그라운드 scenePhase에 따라서 업데이트
-        locationManager.startUpdatingLocation()
+        startBackgroundLocationUpdates()
     }
 }
 
+// MARK: - CLLocationManagerDelegate 시그니처 메서드
 extension LocationsHandler: CLLocationManagerDelegate {
-    private func processLocation(_ current: CLLocation) {
-        guard let lastLocation else {
-            lastLocation = current
-            return
-        }
-        var speed = current.speed
-        if speed < 0 {
-            speed = lastLocation.distance(from: current) / (current.timestamp.timeIntervalSince(lastLocation.timestamp))
-        }
-        kilometerPerHour = speed * 3.6
-        print(speed)
-        self.lastLocation = current
-    }
-    
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         self.authorizationStatus = manager.authorizationStatus
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        if let newLocation = locations.last {
-//            lastLocation = newLocation
-//        }
         for location in locations {
-            processLocation(location)
+            caculateCurrentSpeed(location)
         }
     }
     
