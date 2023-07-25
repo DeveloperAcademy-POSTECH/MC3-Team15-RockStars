8f788396224cc62580de2a0814b84e6d12a54610
Damagucci-Juice <jsr9874@gmail.com>
2023-07-25 21:10:46 +0900

    [refactor][#53] 사용하지 않는 메서드 제거

--- DrivePal/View/VelocityView.swift
+++ DrivePal/View/VelocityView.swift
@@ -22,18 +22,6 @@ struct VelocityView: View {
             }
         }
         .padding(.bottom, 30)
-        .onChange(of: locationHandler.authorizationStatus, perform: updateMessage)
-    }
-    
-    private func updateMessage(_ current: AuthorizationStatus) {
-        switch current {
-        case .success:
-            message = "km/h: \(locationHandler.kilometerPerHour)"
-        case .failure:
-            message = "현재 지역에서 데이터를 읽어오는데 실패했습니다.."
-        case .inProgress:
-            message = "데이터를 읽어오고 있습니다.."
-        }
     }
 }
 
