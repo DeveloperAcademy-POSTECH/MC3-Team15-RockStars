5fd198a9ae574a26cdf9a8ce0bd4e747d64081ea
Damagucci-Juice <jsr9874@gmail.com>
2023-07-25 12:15:13 +0900

    [feat][#53] PlaneView 추가

---
+++ DrivePal/View/PlaneView.swift
@@ -0,0 +1,20 @@
+//
+//  PlaneView.swift
+//  DrivePal
+//
+//  Created by Gucci on 2023/07/25.
+//
+
+import SwiftUI
+
+struct PlaneView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlaneView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaneView()
+    }
+}
