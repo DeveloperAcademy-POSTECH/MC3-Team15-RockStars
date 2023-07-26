78754e3712331222534309a107f6bab0f44eadec
Damagucci-Juice <jsr9874@gmail.com>
2023-07-26 12:02:12 +0900

    [configure][#53] 새로운 파일 추가
    - ConvertibleBackgroundView(전환 가능한 배경 뷰)
    -  DrivingStartView(주행 시작 뷰)

--- DrivePal/View/DrivingPalView.swift
+++ DrivePal/View/DrivingPalView.swift
@@ -15,6 +15,7 @@ enum MotionStatus {
 struct DrivingPalView: View {
 
     @State private var showResultAnalysisView = false
+    @State private var showOnboardingView = false
     @StateObject private var locationHandler = LocationsHandler()
     @StateObject private var motionHandler = MotionHandler()
     @EnvironmentObject var liveActivityModel: LiveActivityModel
